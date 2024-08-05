d3b81837be503660a9cc0b0ec0a0d83f2cc41a46
Ginger <gingerginger5cc@gmail.com>
2024-08-04 21:53:28 -0400

    ex10.2 set up eslint

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+<script src="http://192.168.0.106:8097"></script>
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I guess it works now???????
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
